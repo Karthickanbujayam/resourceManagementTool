70e1ce6b40ef5691d7f997f67d8074b431538723
Karthik-Anburaj-ERC <karthik.a@ericsson.com>
2017-05-18 14:38:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# resourceManagementTool" 
---
+++ routes/tasks.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://dataio:dataio@ds161410.mlab.com:61410/dataio', ['usersAuth']);
+
+// Get All Tasks
+router.get('/tasks', function(req, res, next){
+    db.usersAuth.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+// Get Single Task
+router.get('/task/:id', function(req, res, next){
+    db.usersAuth.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+module.exports = router;
